243ed497006c640a375aea408a61efa4e977d95d
+ <moses.mutitu@student.moringaschool.com>
2025-04-15 19:27:07 +0300

    complere

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Links() {
+  return (
+      <div>Links
+          <a href=''>https://github.com/liza</a>
+          <a href=''>https://www.linkedin.com/in/liza/</a>
+    </div>
+  )
+}
+export default Links
